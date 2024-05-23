a85216fdbfafdd18e3f44f0cdbf8b23340d9bac1
Tyler Menezes <x@tyler.vc>
2024-05-22 20:15:25 -0700

    Update grants

--- src/pages/api/cv.tex.tsx
+++ src/pages/api/cv.tex.tsx
@@ -16,7 +16,7 @@ function* cvListItemToTex(
   if (item.interval) yield item.interval;
   else if (item.date) yield item.date;
   
-  if (item.role && item.company) yield `\\textbf{${item.role} at ${item.company}}`;
+  if (item.role && item.company) yield `\\textbf{${item.role}, ${item.company}}`;
   if (item.degree && item.school) yield `${item.degree}, ${item.school}`;
   if (item.title) yield `\\textbf{\`\`${item.title}''}`;
 
@@ -24,12 +24,10 @@ function* cvListItemToTex(
   if (item.venue) yield item.venue;
 
   if (item.blurb && (item as { type: string }).type === 'role') {
-    yield ' \\\\ \\begin{sublist}' + item.blurb
-      .replace(/ *[\-\*] +/g, '\\item ')
-      .replace(/\n\w*$/, '')
-      .replace(/\n/g, '')
-      + '\\end{sublist}'
-  };
+    yield ' \\\\\n\t\\begin{sublist}\n' + item.blurb.trim()
+      .replace(/ *[\-\*] +/g, `\t\\item `)
+      + '\n\t\\end{sublist}'
+  } else if (item.blurb) yield item.blurb;
 }
 
 function cvListToTex(list: CvList): string {
@@ -41,8 +39,11 @@ function cvListToTex(list: CvList): string {
         .replace(/\&/g, '\\&')
         .replace(/\$/g, '\\$')
         .replace(/\%/g, '\\%')
+        .replace(/\#/g, '\\#')
         .replace(/(https?:\/\/[\w\.\/\?]+)/g, '\\href{$1}{$1}')
+        .trim()
     )
+    .map(i => ['.', '}'].includes(i.slice(-1)) ? i : `${i}.`)
     .join('\n');
 }
 
@@ -65,21 +66,37 @@ export default async function handler(
       items: entries.publications,
     },
     {
-      title: 'Service \\& Outreach',
-      items: entries.volunteering,
+      title: 'Grants',
+      items: entries.grants,
+    },
+    {
+      title: 'Professional Service',
+      items: entries.service,
     },
     {
       title: 'Lectures, Talks, Workshops',
       items: entries.talksInterviews,
     },
     {
+      title: 'Affiliations',
+      items: entries.affiliations,
+    },
+    {
       title: 'Press',
       items: entries.press,
+    },
+    {
+      title: 'Volunteering',
+      items: entries.volunteering,
     },
   ];
 
   const texBody = sections
-    .map(({ title, items }) => `\\section{${title}}\n\\begin{cvlist}\n${cvListToTex(items)}\n\\end{cvlist}`)
+    .map(({ title, items }) => {
+      const itemTex = cvListToTex(items).trim();
+      if (itemTex.length === 0) return '';
+      return `\\section{${title}}\n\\begin{cvlist}\n${itemTex}\n\\end{cvlist}`;
+    })
     .join(`\n\n`);
 
   res.setHeader('Content-type', 'text/plain').send(texBody);
--- src/utils/obsidian/cv.ts
+++ src/utils/obsidian/cv.ts
@@ -7,8 +7,8 @@ import clientConfig from '@/config/clientConfig';
 
 export interface WorkExperience { role: string | null, company: string | null, interval: string | null, blurb: string | null };
 function parseWorkExperience(content: Content[]): WorkExperience {
-  const role = treeToString([content[0]]);
-  const [company, interval] = treeToString([content[1]]).split(', ').map(e => e.trim());
+  const company = treeToString([content[0]]);
+  const [role, interval] = treeToString([content[1]]).split(' , ').map(e => e.trim());
   const blurb = renderMarkdown(valuesRecursive(content.slice(2)));
   return { role, company, interval, blurb };
 }
@@ -78,6 +78,19 @@ function parseTalks(content: Content[]): Talk {
   return { url, title, venue, date, recommended };
 }
 
+export interface Grant { title: string | null, sponsor: string | null, date: string | null, recommended: boolean };
+function parseGrants(content: Content[]): Grant {
+  const link = content[0].type === 'heading' && (content[0].children as Content[])?.filter(c => c.type === 'link')[0];
+  const url = link && link.type === 'link' && link.url || null;
+  const title = treeToString([content[0]]);
+
+  const infoNode = content[1].type === 'paragraph' && content[1];
+
+  const [sponsor, date] = infoNode ? treeToString(infoNode.children.slice(0, 2)).split(', ').map(e => e.trim()) : [];
+  const recommended = infoNode ? treeToString(infoNode.children.slice(3)).toLowerCase().trim().includes('recommended') : false;
+  return { title, sponsor, date, recommended };
+}
+
 export type SkillBlock = [(string | null), (string | null)[] | null];
 type WithType<T, U> = T & { type: U };
 export interface Cv {
@@ -86,10 +99,13 @@ export interface Cv {
   roles: WorkExperience[]
   education: Education[]
   volunteering: WorkExperience[]
+  service: WorkExperience[]
+  affiliations: WorkExperience[]
   publications: Publication[]
   press: Press[]
   openSource: OpenSource[]
   talksInterviews: Talk[]
+  grants: Grant[]
 }
 export type CvList = Cv[keyof Omit<Cv, 'bio' | 'skills'>];
 
@@ -125,8 +141,11 @@ export async function fetchCv(onlyRecommended = false): Promise<Cv> {
   const roles = Object.values(mdParsed['Work Experience'] || {}).slice(1).map(parseWorkExperience);
   const education = Object.values(mdParsed['Education'] || {}).slice(1).map(parseEducation);
   const volunteering = Object.values(mdParsed['Volunteering'] || {}).slice(1).map(parseWorkExperience);
+  const service = Object.values(mdParsed['Professional Service'] || {}).slice(1).map(parseWorkExperience);
+  const affiliations = Object.values(mdParsed['Affiliations and Professional Organizations'] || {}).slice(1).map(parseWorkExperience);
   const publications = Object.values(mdParsed['Publications'] || {}).slice(1).map(parsePublication).filter(filterRecommended);
   const press = Object.values(mdParsed['Press Coverage'] || {}).slice(1).map(parsePress).filter(filterRecommended);
+  const grants = Object.values(mdParsed['Research Grants'] || {}).slice(1).map(parseGrants).filter(filterRecommended);
   const openSource = Object.values(mdParsed['Public Work'] || {}).slice(1).map(parseOpenSource);
   const talksInterviews = Object.values(mdParsed['Talks & Interviews'] || {}).slice(1).map(parseTalks).filter(filterRecommended);
 
@@ -140,5 +159,8 @@ export async function fetchCv(onlyRecommended = false): Promise<Cv> {
     press: addTypeKeys(press, 'press'),
     openSource: addTypeKeys(openSource, 'openSource'),
     talksInterviews: addTypeKeys(talksInterviews, 'talk'),
+    service: addTypeKeys(service, 'service'),
+    grants: addTypeKeys(grants, 'grants'),
+    affiliations: addTypeKeys(affiliations, 'affiliations'),
   };
 }
