9700c1ff3ff41bad743f178014c5a5ecfe5676ec
Tyler Menezes <x@tyler.vc>
2026-04-11 14:01:17 -0700

    Add CV

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,29 +1,43 @@
-import { Container, Box, Heading, Text, Link, Button, useColorModeValue, Flex } from '@chakra-ui/react';
-import { ReactNode } from 'react';
-import Head from 'next/head';
-import { DateTime } from 'luxon';
+import {
+  Container,
+  Box,
+  Heading,
+  Text,
+  Link,
+  Button,
+  useColorModeValue,
+  Flex,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+import Head from "next/head";
+import { DateTime } from "luxon";
 
 export interface PageProps {
-  title?: string
-  credits?: string
-  children: ReactNode
-  noIndex?: boolean
+  title?: string;
+  credits?: string;
+  children: ReactNode;
+  noIndex?: boolean;
 }
 
 export function Page({ title, credits, children, noIndex }: PageProps) {
-  const bg = useColorModeValue('white', 'black');
-  const fg = useColorModeValue('black', 'white');
-  const light = useColorModeValue('gray.400', 'whiteAlpha.400');
+  const bg = useColorModeValue("white", "black");
+  const fg = useColorModeValue("black", "white");
+  const light = useColorModeValue("gray.400", "whiteAlpha.400");
 
   return (
     <>
       <Head>
-        <title>{title ? `${title} / Tyler Menezes` : 'Tyler Menezes'}</title>
+        <title>{title ? `${title} / Tyler Menezes` : "Tyler Menezes"}</title>
         {noIndex && <meta name="robots" content="noindex" />}
       </Head>
       <Box position="sticky" w="100%" top={0} zIndex={1000} mt={1} bg={bg}>
         <Container maxW="container.md">
-          <Flex justifyContent="space-between" borderBottomWidth={2} borderBottomColor={fg} pb={1}>
+          <Flex
+            justifyContent="space-between"
+            borderBottomWidth={2}
+            borderBottomColor={fg}
+            pb={1}
+          >
             <Heading
               as="a"
               href="/"
@@ -56,23 +70,33 @@ export function Page({ title, credits, children, noIndex }: PageProps) {
               >
                 Notes
               </Link>
+              <Link fontSize="md" href="/meet" fontFamily="monospace">
+                Meet
+              </Link>
               <Link
                 fontSize="md"
-                href="/meet"
+                href="https://svc.tyler.vc/cv.pdf"
+                target="_blank"
+                rel="noreferrer"
                 fontFamily="monospace"
               >
-                Meet
+                CV
               </Link>
             </Box>
           </Flex>
         </Container>
       </Box>
-      <Box>
-        {children}
-      </Box>
-      <Container maxW="container.md" mt={8} mb={8} color={light} fontSize="sm" fontFamily="monospace">
+      <Box>{children}</Box>
+      <Container
+        maxW="container.md"
+        mt={8}
+        mb={8}
+        color={light}
+        fontSize="sm"
+        fontFamily="monospace"
+      >
         &copy; 2006&mdash;{DateTime.now().year} Tyler Menezes. {credits}
       </Container>
     </>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,34 @@
-import { Markdown, Page, CvSummaryEntry, PressPhotoChooser, MusicPreview, SmallSubheading } from '@/components';
-import { TravelMap } from '@/components/TravelMap';
-import clientConfig from '@/config/clientConfig';
-import { Rfc, Music, Trip } from '@/utils';
-import { Cv, CvList } from '@/utils/cv';
-import { Box, Container, Divider, Grid, Heading, Image, Link, useColorModeValue } from '@chakra-ui/react';
-import { GetStaticProps } from 'next';
+import {
+  Markdown,
+  Page,
+  CvSummaryEntry,
+  PressPhotoChooser,
+  MusicPreview,
+  SmallSubheading,
+} from "@/components";
+import { TravelMap } from "@/components/TravelMap";
+import clientConfig from "@/config/clientConfig";
+import { Rfc, Music, Trip } from "@/utils";
+import { Cv, CvList } from "@/utils/cv";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Heading,
+  Image,
+  Link,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { GetStaticProps } from "next";
 
 const CHECK_TAG = `#${clientConfig.obsidian.blogTag}`;
 
 export interface IndexProps {
-  cv: Cv,
-  rfcs: Rfc[],
-  music: Music,
-  trips: Trip[],
+  cv: Cv;
+  rfcs: Rfc[];
+  music: Music;
+  trips: Trip[];
 }
 
 function sortRecommended(a: CvList[number], b: CvList[number]) {
@@ -22,93 +38,77 @@ function sortRecommended(a: CvList[number], b: CvList[number]) {
 }
 
 export default function Index({ rfcs, cv, music, trips }: IndexProps) {
-  const light = useColorModeValue('gray.400', 'whiteAlpha.400');
+  const light = useColorModeValue("gray.400", "whiteAlpha.400");
 
   return (
     <Page>
       <Container maxW="container.lg" mt={8} mb={8}>
-        <Image src="https://u.tyler.vc/blog-images/index.jpg" w="100%" alt="" mb={8} />
-        <Grid templateColumns={{ base: '1fr', md: '12fr 5fr' }} gap={8} mb={8}>
+        <Image
+          src="https://u.tyler.vc/blog-images/index.jpg"
+          w="100%"
+          alt=""
+          mb={8}
+        />
+        <Grid templateColumns={{ base: "1fr", md: "12fr 5fr" }} gap={8} mb={8}>
           <Box>
             {cv.bio && <Markdown content={cv.bio} />}
 
             <Box mt={6}>
-              <Heading as="h2" fontSize="xl" mb={2}>Press Photos</Heading>
+              <Heading as="h2" fontSize="xl" mb={2}>
+                Press Photos
+              </Heading>
               <PressPhotoChooser
-                templateColumns={{ base: `repeat(4, 1fr)`, md: `repeat(8, 1fr)` }}
+                templateColumns={{
+                  base: `repeat(4, 1fr)`,
+                  md: `repeat(8, 1fr)`,
+                }}
                 photos={[
-                  { image: '/press-photos/press_4_sm.jpg', url: '/press-photos/press_4.jpg', credit: 'Eclipse Foundation (CC-ND 2.0)' },
-                  { image: '/press-photos/press_1_sm.jpg', url: '/press-photos/press_1.jpg', credit: 'Erin Sylvester' },
-                  { image: '/press-photos/press_2_sm.jpg', url: '/press-photos/press_2.jpg', credit: 'Amelia Bertozzi-Villa' },
+                  {
+                    image: "/press-photos/press_4_sm.jpg",
+                    url: "/press-photos/press_4.jpg",
+                    credit: "Eclipse Foundation (CC-ND 2.0)",
+                  },
+                  {
+                    image: "/press-photos/press_1_sm.jpg",
+                    url: "/press-photos/press_1.jpg",
+                    credit: "Erin Sylvester",
+                  },
+                  {
+                    image: "/press-photos/press_2_sm.jpg",
+                    url: "/press-photos/press_2.jpg",
+                    credit: "Amelia Bertozzi-Villa",
+                  },
                 ]}
               />
             </Box>
           </Box>
           <Box>
-            <Heading as="h2" fontSize="xl" mb={2}>Notes/Essays/RFCs</Heading>
-            {rfcs.filter(r => !r.unlisted).map(e => (
-              <CvSummaryEntry
-                key={e.slug}
-                url={e.slug}
-                title={e.title}
-                date={e.createdAt}
-                fontSize="lg"
-                target="_self"
-              />
-            ))}
+            <Heading as="h2" fontSize="xl" mb={2}>
+              Notes/Essays/RFCs
+            </Heading>
+            {rfcs
+              .filter((r) => !r.unlisted)
+              .map((e) => (
+                <CvSummaryEntry
+                  key={e.slug}
+                  url={e.slug}
+                  title={e.title}
+                  date={e.createdAt}
+                  fontSize="lg"
+                  target="_self"
+                />
+              ))}
           </Box>
         </Grid>
 
         <Divider mt={8} mb={8} />
 
-        <Grid templateColumns={{ base: '1fr', md: 'repeat(3, 1fr)' }} gap={8}>
+        <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} gap={8}>
           <Box>
-            <Heading as="h2" fontSize="xl" mb={4}>Talks &amp; Interviews</Heading>
-            {cv.talksInterviews.sort(sortRecommended).map(e => (
-              <CvSummaryEntry
-                key={e.title}
-                title={e.title}
-                url={e.url}
-                info={e.venue}
-                date={e.date}
-                recommended={e.recommended}
-                details={!e.recommended}
-                fontSize={{ base: 'lg', md: 'md'}}
-              />
-            ))}
-          </Box>
-          <Box>
-            <Heading as="h2" fontSize="xl" mb={4}>Research Grants</Heading>
-            {cv.grants.filter(e => !e.title!.toLowerCase().includes('submitted')).sort(sortRecommended).map(e => (
-              <CvSummaryEntry
-                key={e.title}
-                title={e.title}
-                url={e.url}
-                info={e.sponsor}
-                date={e.date}
-                recommended={e.recommended}
-                details={!e.recommended}
-                fontSize={{ base: 'lg', md: 'md'}}
-              />
-            ))}
-
-            <Heading as="h2" fontSize="xl" mt={8} mb={4}>Press</Heading>
-            {cv.press.sort(sortRecommended).map(e => (
-              <CvSummaryEntry
-                key={e.title}
-                title={e.title}
-                url={e.url}
-                info={e.outlet}
-                date={e.date}
-                recommended={e.recommended}
-                details={!e.recommended}
-                fontSize={{ base: 'lg', md: 'md'}}
-              />
-            ))}
-          </Box>
-          <Box>
-            <Heading as="h2" fontSize="xl" mb={4}>Publications</Heading>
-            {cv.publications.sort(sortRecommended).map(e => (
+            <Heading as="h2" fontSize="xl" mb={4}>
+              Publications
+            </Heading>
+            {cv.publications.sort(sortRecommended).map((e) => (
               <CvSummaryEntry
                 key={e.title}
                 title={e.title}
@@ -117,7 +117,60 @@ export default function Index({ rfcs, cv, music, trips }: IndexProps) {
                 date={e.date}
                 recommended={e.recommended}
                 details={!e.recommended}
-                fontSize={{ base: 'lg', md: 'md'}}
+                fontSize={{ base: "lg", md: "md" }}
+              />
+            ))}
+          </Box>
+          <Box>
+            <Heading as="h2" fontSize="xl" mb={4}>
+              Research Grants
+            </Heading>
+            {cv.grants
+              .filter((e) => !e.title!.toLowerCase().includes("submitted"))
+              .sort(sortRecommended)
+              .map((e) => (
+                <CvSummaryEntry
+                  key={e.title}
+                  title={e.title}
+                  url={e.url}
+                  info={e.sponsor}
+                  date={e.date}
+                  recommended={e.recommended}
+                  details={!e.recommended}
+                  fontSize={{ base: "lg", md: "md" }}
+                />
+              ))}
+
+            <Heading as="h2" fontSize="xl" mt={8} mb={4}>
+              Press
+            </Heading>
+            {cv.press.sort(sortRecommended).map((e) => (
+              <CvSummaryEntry
+                key={e.title}
+                title={e.title}
+                url={e.url}
+                info={e.outlet}
+                date={e.date}
+                recommended={e.recommended}
+                details={!e.recommended}
+                fontSize={{ base: "lg", md: "md" }}
+              />
+            ))}
+          </Box>
+          <Box>
+            <Heading as="h2" fontSize="xl" mb={4}>
+              Talks &amp; Interviews
+            </Heading>
+            {cv.talksInterviews.sort(sortRecommended).map((e) => (
+              <CvSummaryEntry
+                key={e.title}
+                title={e.title}
+                url={e.url}
+                info={e.venue}
+                date={e.date}
+                recommended={e.recommended}
+                details={!e.recommended}
+                fontSize={{ base: "lg", md: "md" }}
               />
             ))}
           </Box>
@@ -125,14 +178,17 @@ export default function Index({ rfcs, cv, music, trips }: IndexProps) {
 
         <Divider mt={8} mb={8} />
 
-
-        <Heading as="h2" fontSize="xl" mb={4}>Personal</Heading>
-        <Grid templateColumns={{ base: '1fr', md: '5fr 12fr' }} gap={8}>
+        <Heading as="h2" fontSize="xl" mb={4}>
+          Personal
+        </Heading>
+        <Grid templateColumns={{ base: "1fr", md: "5fr 12fr" }} gap={8}>
           <Box>
             <SmallSubheading mb={2}>Recently Listening:</SmallSubheading>
             <MusicPreview music={music.weeklyAlbum} />
 
-            <SmallSubheading mt={4} mb={2}>Most Listened:</SmallSubheading>
+            <SmallSubheading mt={4} mb={2}>
+              Most Listened:
+            </SmallSubheading>
             <MusicPreview music={music.overallAlbum} rows={3} />
           </Box>
 
@@ -141,48 +197,48 @@ export default function Index({ rfcs, cv, music, trips }: IndexProps) {
             <TravelMap
               trips={[
                 ...trips,
-                { country: 'CA', state: 'British Columbia' },
-                { country: 'CA', state: 'Alberta' },
-                { country: 'CA', state: 'Ontario' },
-                { country: 'CA', state: 'Quebec' },
-                { country: 'CA', state: 'Manitoba' },
-                { country: 'GB', state: '' },
-                { country: 'TT', state: '' },
-                { country: 'QA', state: '' },
-                { country: 'US', state: 'ID' },
-                { country: 'US', state: 'CO' },
-                { country: 'US', state: 'WY' },
-                { country: 'US', state: 'KS' },
-                { country: 'US', state: 'WI' },
-                { country: 'US', state: 'AK' },
-                { country: 'US', state: 'HI' },
-                { country: 'US', state: 'MO' },
-                { country: 'US', state: 'MN' },
-                { country: 'US', state: 'MD' },
-                { country: 'US', state: 'DE' },
-                { country: 'US', state: 'WV' },
-                { country: 'US', state: 'NJ' },
-                { country: 'US', state: 'NH' },
-                { country: 'US', state: 'ME' },
-                { country: 'US', state: 'CT' },
-                { country: 'US', state: 'TN' },
-                { country: 'US', state: 'IN' },
-                { country: 'US', state: 'NM' },
+                { country: "CA", state: "British Columbia" },
+                { country: "CA", state: "Alberta" },
+                { country: "CA", state: "Ontario" },
+                { country: "CA", state: "Quebec" },
+                { country: "CA", state: "Manitoba" },
+                { country: "GB", state: "" },
+                { country: "TT", state: "" },
+                { country: "QA", state: "" },
+                { country: "US", state: "ID" },
+                { country: "US", state: "CO" },
+                { country: "US", state: "WY" },
+                { country: "US", state: "KS" },
+                { country: "US", state: "WI" },
+                { country: "US", state: "AK" },
+                { country: "US", state: "HI" },
+                { country: "US", state: "MO" },
+                { country: "US", state: "MN" },
+                { country: "US", state: "MD" },
+                { country: "US", state: "DE" },
+                { country: "US", state: "WV" },
+                { country: "US", state: "NJ" },
+                { country: "US", state: "NH" },
+                { country: "US", state: "ME" },
+                { country: "US", state: "CT" },
+                { country: "US", state: "TN" },
+                { country: "US", state: "IN" },
+                { country: "US", state: "NM" },
               ]}
             />
           </Box>
         </Grid>
       </Container>
     </Page>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const [cv, rfcs, music, trips] = await Promise.all([
-    await fetch('https://svc.tyler.vc/cv.json').then(r => r.json()),
-    await fetch('https://svc.tyler.vc/rfcs.json').then(r => r.json()),
-    await fetch('https://svc.tyler.vc/music.json').then(r => r.json()),
-    await fetch('https://svc.tyler.vc/trips.json').then(r => r.json()),
+    await fetch("https://svc.tyler.vc/cv.json").then((r) => r.json()),
+    await fetch("https://svc.tyler.vc/rfcs.json").then((r) => r.json()),
+    await fetch("https://svc.tyler.vc/music.json").then((r) => r.json()),
+    await fetch("https://svc.tyler.vc/trips.json").then((r) => r.json()),
   ]);
 
   return {
@@ -194,4 +250,4 @@ export const getStaticProps: GetStaticProps = async () => {
     },
     revalidate: 300,
   };
-}
+};
